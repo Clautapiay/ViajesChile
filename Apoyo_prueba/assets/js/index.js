8925981c3f882f7c0c7dce3a486c3d8e8029759c
Claudiatapiay <ctapia.yelpi@gmail.com>
2024-02-19 18:15:53 -0300

    Ultimo commit, se realizaron todos los cambios necesarios

--- Apoyo_prueba/assets/js/index.js
+++ Apoyo_prueba/assets/js/index.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("#Enviar").click(function () {
-alert("Su mensaje ha sido enviado correctamente, pronto se contactarán con usted.")
-});
+    $("#Enviar").click(function () {
+        alert("Su mensaje ha sido enviado correctamente, pronto se contactarán con usted.")
+    });
 
+
+    $("#titulo").click(function () {
+        alert("¡Visitanos de manera presencial y tendrás un 20% de descuento en tu próximo viaje! Pide nuestra drección por medio de CONTACTANOS")
+    });
 
 })
 
